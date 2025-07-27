1bce4775837e6994ad5b1ade116fd2d2b6349303
Indam <indam.muhammad@gmail.com>
2025-07-28 01:50:26 +0700

    update the graphql uri

--- src/lib/apollo/apollo-client.ts
+++ src/lib/apollo/apollo-client.ts
@@ -3,7 +3,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI || 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 });
 
